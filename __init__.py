5fc6e24f01cf2f623d105a4642808a17d3641d31
Jared Forsyth <jared@jaredforsyth.com>
2014-02-07 10:21:03 -0700

    now with unified compilation!

---
+++ __init__.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python
+
+from frame_getter import *
+
+
+# vim: et sw=4 sts=4
--- setup.py
+++ setup.py
@@ -2,11 +2,27 @@
 from distutils.core import setup
 from distutils.extension import Extension
 from Cython.Distutils import build_ext
+import numpy
+
+import subprocess
+
+LINK = subprocess.check_output('pkg-config --libs libavformat libavcodec libswscale libavutil sdl'.split()) + ' -lm'
+COMP = subprocess.check_output('pkg-config --cflags libavformat libavcodec libswscale libavutil sdl'.split())
 
 setup(
+    name = 'ffmpy',
     cmdclass = {'build_ext': build_ext},
     ext_modules = [
-        Extension("frame_getter", ["frame_getter.pyx"], libraries=['ffmpy'])
+        Extension(
+            name="frame_getter",
+            sources=["frame_getter.pyx", 'ffmpy.c'],
+            include_dirs=[numpy.get_include()],
+            language='c',
+            extra_compile_args = COMP.split(),
+            extra_link_args = LINK.split()
+
+            # libraries=['ffmpy']
+        )
     ]
 )
 
